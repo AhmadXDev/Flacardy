5209f3bc22692c7a7f5386e7f1253fa249e18c11
Ahmad <ahmad.yasarg@gmail.com>
2024-12-21 18:32:44 +0300

    added Drift for database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:flacardy/data/database.dart';
 import 'package:flacardy/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = AppDatabase();
+
+  await database.into(database.todoItems).insert(TodoItemsCompanion.insert(
+        title: 'todo: finish drift setup',
+        content: 'We can now write queries and define our own tables.',
+      ));
+  List<TodoItem> allItems = await database.select(database.todoItems).get();
+
+  print('items in database: $allItems');
 }
 
 class MainApp extends StatelessWidget {
